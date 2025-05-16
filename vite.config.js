758cb29ec02c599ae8ddf4c7208e47378018fb6b
LyrinePoliarco <152787755+LyrinePoliarco@users.noreply.github.com>
2025-05-16 21:22:42 +0800

    main

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or vue, or whatever framework you're using
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  // Ensure base path is set correctly (usually '/' for Vercel)
-  base: '/',
+
 })
